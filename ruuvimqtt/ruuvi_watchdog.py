6a5efae1c5487ab81cdb9f0f224c86e52d91949e
Mikael Mätäsaho <mikael.matasaho@digia.com>
2025-07-30 13:47:37 +0300

    some cleanup

--- ruuvimqtt/ruuvi_watchdog.py
+++ ruuvimqtt/ruuvi_watchdog.py
@@ -7,7 +7,6 @@ import json
 from paho.mqtt import client as mqtt_client
 import datetime
 from datetime import date
-from time import gmtime
 import logging
 from queue import Queue
 import sys
@@ -27,73 +26,8 @@ _queue = Queue()
 
 _logging_level =logging.DEBUG
 _logfile_name ="ruuvi_watchdog.log"
-
-_sensors = {"C0:E7:B2:DD:8B:1A" : "Fence",
-	"D9:27:7C:28:2F:E6" : "Mobile",
-	"EC:07:DA:3E:5F:F2" : "Freezer",
-	"D6:EC:67:41:9D:76" : "Fridge" }
-
-# initial measurement
-_measurement = [{"measurement": "ruuvi_measurements",
-	"tags": {
-		"dataFormat": 0,
-		"mac": ""
-	},
-	"fields": {
-		"absoluteHumidity" : 0.00,
-		"accelerationAngleFromX" : 0.00,
-		"accelerationAngleFromY" : 0.00,
-		"accelerationAngleFromZ" : 0.00,
-		"accelerationTotal" : 0.00,
-		"accelerationX" : 0.00,
-		"accelerationY" : 0.00,
-		"accelerationZ" : 0.00,
-		"airDensity" : 0.00,
-		"batteryVoltage" : 0.00,
-		"dewPoint" : 0.00,
-		"equilibriumVaporPressure" : 0.00,
-		"humidity" : 0.00,
-		"measurementSequenceNumber" : 0,
-		"movementCounter" : 0,
-		"pressure" : 0.00,
-		"rssi" : 0.00,
-		"temperature" : 0.00,
-		"txPower" : 0.00
-
-	}
-	}
-	]
-
-#def write_to_influx(my_sensor = None, data =None):
-#	if data is None or my_sensor is None:
-#		return
-#
-#	try:
-##		dbClient.write_points(measurement)
-#		measurement[0]['tags']['mac'] =my_sensor
-##data['mac']
-#		measurement[0]['fields']['absoluteHumidity'] =data['humidity']
-#		measurement[0]['fields']['accelerationX'] =data['acceleration_x']
-#		measurement[0]['fields']['accelerationY'] =data['acceleration_y']
-#		measurement[0]['fields']['accelerationZ'] =data['acceleration_z']
-#		measurement[0]['fields']['accelerationTotal'] =data['acceleration']
-#		measurement[0]['fields']['batteryVoltage'] =data['battery']
-#		measurement[0]['fields']['humidity'] =data['humidity']
-#		measurement[0]['fields']['measurementSequenceNumber'] =data['measurement_sequence_number']
-#		measurement[0]['fields']['movementCounter'] =data['movement_counter']
-#		measurement[0]['fields']['pressure'] =data['pressure']
-##		measurement[0]['fields']['rssi'] =data['rssi']
-#		measurement[0]['fields']['temperature'] =data['temperature']
-#		measurement[0]['fields']['txPower'] =data['tx_power']
-#
-#		logger.debug(f"'{measurement}'")
-#
-#		dbClient.write_points(measurement)
-#
-#	except(KeyError):
-#		logger.error(f"Exception --> {KeyError}")
-
 _logger = logging.getLogger(__name__)
+
 logging.basicConfig(level=_logging_level, format ='%(asctime)s: %(name)s: %(levelname)s - %(message)s')
 #logging.basicConfig(level=_logging_level, filename = _logfile_name, format ='%(asctime)s: %(name)s: %(levelname)s - %(message)s')
 
